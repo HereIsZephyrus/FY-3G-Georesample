b8a71e0a923b5b306ac7ff27b0a6cfcb09d5a7fd
Channing Tong <Channing_TongCN@outlook.com>
2025-07-24 16:30:07 +0800

    [WIP] achieve geotransfer functioon

--- include/geotransfer.h
+++ include/geotransfer.h
@@ -1,4 +1,18 @@
 #ifndef GEOTRANSFER_H
 #define GEOTRANSFER_H
+#include <math.h>
+#include <stdbool.h>
+
+#define WGS84_A 6378137.0
+#define WGS84_B 6356752.3142
+#define WGS84_E (sqrt(WGS84_A * WGS84_A - WGS84_B * WGS84_B) / WGS84_A)
+
+double ComputeN(const double latitude);
+double ComputeS(const double t1, const double t2, const double t3, const double t4, const double e, const double r);
+bool IsGeodeticValid(const double latitude, const double longitude, const double height);
+bool TransferGeodeticToCartesian(const double latitude, const double longitude, const double height, double *x, double *y, double *z);
+bool TransferCartesianToGeodetic(const double x, const double y, const double z, double *latitude, double *longitude, double *height, const bool iterative);
+void TransferCartesianToGeodeticLagrange(const double x, const double y, const double z, double *latitude, double *height);
+void TransferCartesianToGeodeticIterative(const double x, const double y, const double z, double *latitude, double *height);
 
 #endif
